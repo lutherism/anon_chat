b757c33e0f7e805d8ae469a4ffbf68a2ac1b690f
Alex Jansen <xanderjanz@gmail.com>
2015-01-09 10:46:41 -0800

    Update stores.js

--- src/js/stores.js
+++ src/js/stores.js
@@ -1,12 +1,50 @@
-define(['backbone', 'dispatcher'], function(
-  Backbone, dispatcher
+define(['backbone', 'dispatcher', 'pathStore', 'collections/Comments',
+  'collections/Threads'], function(
+  Backbone, dispatcher, PathStore, Comments, Threads
   ) {
   var Store = Backbone.Model.extend({
     initialize: function(options) {
       dispatcher.register(this.dispatchHandler.bind(this));
     },
+    requiredThreadRouteStores: {
+      'commentStore': Comments,
+      'pathStore': PathStore,
+      'threadStore': Threads
+    },
     dispatchHandler: function(payload) {
-
+      if (payload.source === dispatcher.constants.ROUTE_ACTION) {
+        if (payload.action.type === 'thread') {
+          this.closeUselessStores(this.requiredThreadRouteStores);
+          this.requiredThreadRouteStores.map(function(key, val) {
+            this.ensureCreated(key, val);
+          });
+        }
+      }
+    },
+    handleRfisRoute: function(payload) {
+      var storesRequired = {
+        'commentStore': Comments,
+        'pathStore': PathStore,
+        'threadStore': Threads
+      };
+      storerRequired.map(function(key, val) {
+        this.ensureCreated(key, val)
+      });
+    },
+    closeUselessStores: function(new) {
+      this.map(function (key, val) {
+        if (!new[key]) {
+          this.get(key).destroy();
+          this.unset(key);
+          this.trigger('change:emit');
+        }
+      })
+    },
+    ensureCreated: function(prop, cons) {
+      if (!this.get(prop)) {
+        this.set(prop, new cons());
+        this.trigger('change:emit');
+      }
     },
     getComments: function() {
       return this.get('commentStore');
